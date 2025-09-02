88bcd3ceaae6fb56a0fa0a76ccff202468f252f0
klopez-2024401 <klopez-2024401@kinal.edu.gt>
2025-09-01 19:44:47 -0600

    se agrego

---
+++ src/main/java/org/kaven/A42_Grupal_ClinaDental/A42GrupalClinaDentalApplication.java
@@ -0,0 +1,13 @@
+package org.kaven.A42_Grupal_ClinaDental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A42GrupalClinaDentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A42GrupalClinaDentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kaven/A42_Grupal_ClinaDental/A42GrupalClinaDentalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kaven.A42_Grupal_ClinaDental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A42GrupalClinaDentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
